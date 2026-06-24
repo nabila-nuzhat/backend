80e45f1b1589bb90e08d154eb64f01e52f856190
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2026-06-24 12:40:00 +0600

    Buffer.from() hex convert/ encoding: difference?, why blank output?

--- node-js-task/buffer/index.js
+++ node-js-task/buffer/index.js
@@ -4,3 +4,16 @@
  */
 const buff = Buffer.alloc(10);
 console.log(buff); // <Buffer 00 00 00 00 00 00 00 00 00 00> 
+
+const buffFrom = Buffer.from("hola");
+console.log("buffFrom", buffFrom); // buffFrom <Buffer 68 6f 6c 61>
+
+const buffFrom2 = Buffer.from("Adios! Hola Amigos", "hex");
+console.log("buffFrom2", buffFrom2); // buffFrom2 <Buffer ad>
+
+const buffFrom3 = Buffer.from("hello", "hex");
+console.log("buffFrom3", buffFrom3); // buffFrom3 <Buffer >
+
+const buffFrom4 = Buffer.from("Adios", "hex");
+console.log("buffFrom4", buffFrom4); // buffFrom4 <Buffer ad>
+
