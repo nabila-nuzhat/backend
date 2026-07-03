68ad6856f99291de240376a7ad8cddc78bf45627
nabila-nuzhat <e.nabilanuzhat@gmail.com>
2026-07-03 10:11:51 +0600

    bigFoot file was created for test deleted to avoid git push issue over 10 MB

--- node-js-task/stream/index.js
+++ node-js-task/stream/index.js
@@ -18,8 +18,9 @@
 
     // // const fs = require('fs'); //  for type commonJs
     // // import fs from "fs" // how this too work? ISSUE solved (claude suggetsion "Node.js Package.json type field compatibility syntax code")
-   import fs from "node:fs"
-   import http from "node:http"
+    import { log } from "node:console";
+    import fs from "node:fs"
+    import http from "node:http"
        // // const server = require('http').createServer(); // for type commonJs
 
        const server = http.createServer();
@@ -33,3 +34,29 @@
 
         server.listen(3000);
         // server.listen(8000);
+
+
+/* SERVER 2:  using create Stream for request response stream  connecting with pipe
+BIGGER file around 100 MB
+********* bigFoot file DELETED as size was over 10MB to avoid github push issue !!!!!!!!!!!!!!!! ============================ =============================== 
+         */
+
+        // const file = fs.createWriteStream('./bigFoot.file');
+
+        // for(let i=0; i<= 1e5; i++) {
+        //       file.write('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n');
+        //     // file.write(' Ami Home Work korte bhulbo na.\n');
+        // }
+
+        // file.end();
+
+        // const server2 = http.createServer();
+        //  server2.on('request', (req, res) => {
+        //     const src = fs.createReadStream("./bigFoot.file")
+        //     src.pipe(res);
+        //  })
+
+// console log Server 2 running
+        //  server2.listen(8000, () =>{
+        //     console.log("Server 2 is running bigFoot file on http://localhost:8000");
+        //  });
